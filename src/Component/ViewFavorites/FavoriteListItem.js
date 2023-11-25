7150fa7b27b141b68816b74c4112bd0871ba12f9
Md <md1040582@gmail.com>
2023-11-25 19:03:05 +0600

    ...

--- src/Component/ViewFavorites/FavoriteListItem.js
+++ src/Component/ViewFavorites/FavoriteListItem.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const FavoriteListItem = ({ favorite, handleRemoveFavorite }) => {
+  console.log(favorite)
   return (
     <li className='w-[250px] cursor-pointer border border-[whitesmoke] hover:scale-[1.01] bg-white relative shadow rounded-md px-3 py-6 m-2'>
     <h3 className='text-left capitalize'><strong>Word: </strong>{favorite.word}</h3>
+    <h3 className='text-left capitalize'><strong>Type: </strong>{favorite.partOfSpeech}</h3>
     {favorite?.definition?.map((item, i) => (
       <div key={i}>
         <p className='text-left'><strong>Definition: </strong>{item.definition}</p>
